fede5ec4989488a4f4fb6e6ed4781252b12fd625
Jeppenseppiro <jeffjurolan990@gmail.com>
2023-05-02 18:23:00 +0800

    vue laravel echo

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -5,13 +5,23 @@
 </template>
 
 <script>
+import { onMounted, ref, computed, watch } from "vue";
 import Map from "../components/Map.vue";
+
 export default {
   props: {
-    name: String,
+    piData: Object,
   },
   components: {
     Map,
+  },
+  setup(props) {
+    onMounted(() => {});
+
+    // New Map Coordinates watcher
+    window.Echo.channel("piData").listen("SendPiData", (newPiData) => {
+      console.log(newPiData);
+    });
   },
 };
 </script>
